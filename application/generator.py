16eedcc45817d8d99ec920c95bb28ae3d1a5caf1
sharonkele <sharonkele98@gmail.com>
2019-11-11 15:11:51 +0000

    updated generator.py file

--- application/generator.py
+++ application/generator.py
@@ -1,9 +1,17 @@
-from random import radiant 
-
-def randon_with_N_digits(n):
+from random import randint 
+import string
+#generating random numbers 
+def random_with_N_digits(n):
 	range start = 10**(n-1)
 	range_end = 10**(n-1)
 	return radiant(rage_start, range_end)
 
-print random_with_N_digits(5)
+random_number= random_with_N_digits(2)
+#generates random letters 
+string.ascii_letters
+'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+import random
+def randon_char(y):
+	return ''.join(random.choice(string.ascii_letters) for x in range(y))
+random_letter = (random_char(5))
 

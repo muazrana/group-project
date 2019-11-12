da9fd8cc3b41dd064dbd913095957a31f59bfd02
sharonkele <55387354+sharonkele@users.noreply.github.com>
2019-11-12 15:48:49 +0000

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/flaskapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:flaskranasharon@flaskapp.c14knypx6xhk.eu-west-1.rds.amazonaws.com/flaskapp'
 app.config['SECRET_KEY'] = '7589g2658d55g256582365852r2h65fg'
 
 db = SQLAlchemy(app)

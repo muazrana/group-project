a38d2590ae52ee39fa030c462bfbf4925160916d
sharonkele <sharonkele98@gmail.com>
2019-11-12 10:56:25 +0000

    updated mysql

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,19 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@mysql/flaskapp'
 app.config['SECRET_KEY'] = '7589g2658d55g256582365852r2h65fg'
 
 db = SQLAlchemy(app)
 
 
-from application import routes
+from application import route
+
+
+
+
+
+

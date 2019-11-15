e6ce97438eb8a9f7a6dd113fefbbaaadd0b1c8e4
EC2 Default User <ec2-user@ip-172-31-36-187.eu-west-1.compute.internal>
2019-11-15 15:35:27 +0000

    editing Run.py file for lambda function

--- run.py
+++ run.py
@@ -1,4 +1,26 @@
-from application import app
+from  flask import Flask, render_template, url_for, redirect 
+import boto3
+import json
+import random
+import string 
+
+app= Flask(__name__)
+awsFunction = boto3.client('lambda', eu-west-1'
+
+@app.route("/")
+@app.route("/home")
+def home():
+	username = awsFunction.invoke(
+				   FunctionName='shuffle',
+				   InvocationType='RequestResponse'
+				   )
+	account_id = json.loads(username['Payload'].read().decode("utf-8"))
+
+	userPrize = awsFunction.invoke(
+				    FunctionName='prize_gen',
+				    InvicationType='RequestResponse'
+	prize = json.loads(userPrize['Payload'].read().decode("utf-8"))
+			    )
 
 if __name__ == '__main__':
 		app.run(debug=True, host="0.0.0.0")

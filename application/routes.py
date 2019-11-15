6486daa8a26f09efe02956df2539fe2d0bef3e2f
sharonkele <55387354+sharonkele@users.noreply.github.com>
2019-11-15 17:50:01 +0000

    Added Lambda routes

--- application/routes.py
+++ application/routes.py
@@ -1,55 +1,47 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-from flask import Flask, render_template, url_for, redirect, request
-from application import app, db
-from application.models import User
-from application.forms import NameForm
-import random
 
 from  flask import Flask, render_template, url_for, redirect 
+from app import app, db
+from flask import Flask render_template, url_for, redirect 
 from application.form import NameForm 
+from application.models import Users
 import boto3
 import json
+import boto3
 import random
+from random import randint 
 import string 
+
+awsFunction = boto3.client('lambda', region_name='eu-west-1')
 
 app= Flask(__name__)
 awsFunction = boto3.client('lambda', eu-west-1'
 
-@app.route("/")
-@app.route("/home")
-def home():
-	username = awsFunction.invoke(
-				   FunctionName='shuffle',
-				   InvocationType='RequestResponse'
-				   )
-	account_id = json.loads(username['Payload'].read().decode("utf-8"))
-
-	userPrize = awsFunction.invoke(
-				    FunctionName='prize_gen',
-				    InvicationType='RequestResponse'
-	prize = json.loads(userPrize['Payload'].read().decode("utf-8"))
-	package = send_data(
-			    )
-
-
-
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET','POST'])
 def home():
-	  form = NameForm()
-	  prize_list = ['House', '£10,000', '£100,000', 'Car', 'Mac Computer', 'Ipad', 'Iphone 11 pro', 'Package holiday', '£10','£20', '£50']
-	  prize_won=(random.choice(prize_list))
-	  random_number = str(random_with_N_digits(2))
-	  random_letter = str(random_char(5))
-	  account_id = random_number + random_letter
-	  if form.validate_on_submit(): 
-	  	user = User(name=form.name.data, account_id=account_id, prize_won=prize_won)
-	  	db.session.add(user)
-	  	db.session.commit()
-	  	id = User.query.filter_by(account_id=account_id).first()
-	  	return redirect(url_for('result', id=id.id))
-	  return render_template('home.html', title='Home', form=form)
+     form = NameForm()
+     username = awsFunction.invoke(
+				   FunctionName='shuffle',
+				   InvocationType='RequestResponse'
+				   )
+     account_id = json.loads(username['Payload'].read().decode("utf-8"))
+
+     userPrize = awsFunction.invoke(
+				    FunctionName='prize_gen',
+				    InvicationType='RequestResponse'
+     prize_won = json.loads(userPrize['Payload'].read().decode("utf-8"))
+     if form.validate_on_submit():
+	     User = Users( name = form.name.data, 
+			  account_id = account_id,
+			  prize_won = prize_won) 
+	     db.session.add(User)
+	     db.session.commit()
+	     id = User.query.filter_by(account_id=account_id).first()
+	     return redirect(url_for('result', id=id.id)
+	return render_template('home.html', title='Home', form=form)
+	
 
 @app.route("/result/<int(min=1):id>")
 def result(id):

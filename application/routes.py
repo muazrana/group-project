a7b446282b0ab32bb284e8f111193e5f7fd74b2b
sharonkele <55387354+sharonkele@users.noreply.github.com>
2019-11-15 17:17:45 +0000

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -4,9 +4,33 @@ from flask import Flask, render_template, url_for, redirect, request
 from application import app, db
 from application.models import User
 from application.forms import NameForm
-from application.generator import random_with_N_digits, random_char
 import random
 
+from  flask import Flask, render_template, url_for, redirect 
+from application.form import NameForm 
+import boto3
+import json
+import random
+import string 
+
+app= Flask(__name__)
+awsFunction = boto3.client('lambda', eu-west-1'
+
+@app.route("/")
+@app.route("/home")
+def home():
+	username = awsFunction.invoke(
+				   FunctionName='shuffle',
+				   InvocationType='RequestResponse'
+				   )
+	account_id = json.loads(username['Payload'].read().decode("utf-8"))
+
+	userPrize = awsFunction.invoke(
+				    FunctionName='prize_gen',
+				    InvicationType='RequestResponse'
+	prize = json.loads(userPrize['Payload'].read().decode("utf-8"))
+	package = send_data(
+			    )
 
 
 

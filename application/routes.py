d54df31c8887fafc0158cfdca0e4c5e872e8b5f0
sharonkele <sharonkele98@gmail.com>
2019-11-18 11:05:05 +0000

    fixed routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,33 +1,33 @@
-from app import app, db
 from flask import Flask, render_template, url_for, redirect 
-from app.forms import NameForm 
-from app.models import User
+from application import app, db
+from application.forms import NameForm 
+from application.models import User
 import boto3
 import json
 import random
 from random import randint 
 import string 
 
-awsFunction = boto3.client('lambda', region_name='eu-west-1')
+awsFunction =boto3.client('lambda', region_name='eu-west-1')
 
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET','POST'])
 def home():
 	form = NameForm()
-    username = awsFunction.invoke(FunctionName='shuffle', InvocationType='RequestResponse')
-    account_id = json.loads(username['Payload'].read().decode("utf-8"))
+	username = awsFunction.invoke(FunctionName='shuffle',InvocationType='RequestResponse')
+	account_id = json.loads(username['Payload'].read().decode("utf-8"))
 
-    userPrize = awsFunction.invoke(FunctionName='prize_gen',InvocationType='RequestResponse')
-    prize_won = json.loads(userPrize['Payload'].read().decode("utf-8"))
-    if form.validate_on_submit():
-	    Users = User(name = form.name.data, 
+	userPrize = awsFunction.invoke(FunctionName='prize_gen',InvocationType='RequestResponse')
+	prize_won = json.loads(userPrize['Payload'].read().decode("utf-8"))
+	if form.validate_on_submit():
+		Users = User(name = form.name.data, 
 			  account_id = account_id,
 			  prize_won = prize_won) 
-	    db.session.add(Users)
-	    db.session.commit()
-	    id = User.query.filter_by(account_id=account_id).first()
-	    return redirect(url_for('result', id=id.id)
+		db.session.add(Users)
+		db.session.commit()
+		id = User.query.filter_by(account_id=account_id).first()
+		return redirect(url_for('result', id=id.id))
 	return render_template('home.html', title='Home', form=form)
 	
 

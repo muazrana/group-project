a95451951afe829fa42597f7556b00d897f00e3f
sharonkele <sharonkele98@gmail.com>
2019-11-18 10:23:50 +0000

    updated routes.py file

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from  flask import Flask, render_template, url_for, redirect 
+from flask import Flask, render_template, url_for, redirect 
 from app.forms import NameForm 
 from app.models import User
 import boto3
@@ -14,25 +14,20 @@ awsFunction = boto3.client('lambda', region_name='eu-west-1')
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET','POST'])
 def home():
-     form = NameForm()
-     username = awsFunction.invoke(
-				   FunctionName='shuffle',
-				   InvocationType='RequestResponse'
-				   )
-     account_id = json.loads(username['Payload'].read().decode("utf-8"))
+	form = NameForm()
+    username = awsFunction.invoke(FunctionName='shuffle', InvocationType='RequestResponse')
+    account_id = json.loads(username['Payload'].read().decode("utf-8"))
 
-     userPrize = awsFunction.invoke(
-				    FunctionName='prize_gen',
-				    InvicationType='RequestResponse')
-     prize_won = json.loads(userPrize['Payload'].read().decode("utf-8"))
-     if form.validate_on_submit():
-	     Users = User(name = form.name.data, 
+    userPrize = awsFunction.invoke(FunctionName='prize_gen',InvocationType='RequestResponse')
+    prize_won = json.loads(userPrize['Payload'].read().decode("utf-8"))
+    if form.validate_on_submit():
+	    Users = User(name = form.name.data, 
 			  account_id = account_id,
 			  prize_won = prize_won) 
-	     db.session.add(Users)
-	     db.session.commit()
-	     id = User.query.filter_by(account_id=account_id).first()
-	     return redirect(url_for('result', id=id.id)
+	    db.session.add(Users)
+	    db.session.commit()
+	    id = User.query.filter_by(account_id=account_id).first()
+	    return redirect(url_for('result', id=id.id)
 	return render_template('home.html', title='Home', form=form)
 	
 
